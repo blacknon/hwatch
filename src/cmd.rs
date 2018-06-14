b9debd1c4ed40245c0588dd6321f3f840e8b85e5
uesugi <uesugi@sproutgroup.co.jp>
2018-06-14 19:51:21 +0900

    run command (only)

---
+++ src/cmd.rs
@@ -0,0 +1,26 @@
+use std::process::Command;
+
+pub struct Cmd {
+    pub command: String,
+    pub stdout: String,
+    pub stderr: String
+}
+
+impl Cmd {
+    // run command
+    pub fn run(&mut self) {
+        let output = Command::new("sh")
+                            .arg("-c")
+                            .arg(&self.command)
+                            .output()
+                            .expect("failed to execute process");
+
+        // set var
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let stderr = String::from_utf8_lossy(&output.stderr);
+
+        // set self var
+        self.stdout = stdout.to_string();
+        self.stderr = stderr.to_string();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+
+mod cmd;
+
 fn main() {
-    println!("Hello, world!");
+    let mut command = cmd::Cmd {
+        command: "ls -al ~/".to_string(),
+        stdout: "".to_string(),
+        stderr: "".to_string() 
+    };
+
+    command.run();
+
+    if command.stdout.len() > 0{
+        print!("stdout:\n{}", command.stdout);
+    }
+
+    if command.stderr.len() > 0{
+        println!("stderr:\n{}", command.stderr);
+    }
+
+    let start = SystemTime::now();
+    let since_the_epoch = start.duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    println!("{:?}", since_the_epoch);
 }
