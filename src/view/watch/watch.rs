0493c8fccf620a6919e3310f29d4645ff8e7305d
Blacknon <blacknon@orebibou.com>
2019-04-04 08:03:08 +0900

    add

--- src/view/watch/watch.rs
+++ src/view/watch/watch.rs
@@ -1,18 +1,12 @@
 // @TODO
-//     こちらのファイルではファイルの表示機能だけ制御させるよう、書き換えを行う！
+//     こちらのファイルではwatchpadの表示機能だけ制御させるよう、書き換えを行う！
 //     Structにresultいるか？？いらないんじゃないか？？
 
 // module
 use ncurses::*;
-use std::cmp;
-
-// local module
-use cmd::Result;
 
 #[derive(Clone)]
 pub struct WatchPad {
-    pub result: Result,
-    pub result_diff_output: String,
     pub screen: WINDOW,
     pub pad: WINDOW,
     pub pad_lines: i32,
@@ -23,8 +17,6 @@ impl WatchPad {
     // set default value
     pub fn new(_screen: WINDOW) -> Self {
         Self {
-            result: Result::new(),
-            result_diff_output: String::new(),
             screen: _screen,
             pad: newpad(0, 0),
             pad_lines: 0,
@@ -32,35 +24,14 @@ impl WatchPad {
         }
     }
 
-    // count pad line
-    pub fn set_size(&mut self, output_type: i32) {
-        let mut max_x = 0;
-        let mut max_y = 0;
+    //
+    pub fn print(&mut self, _data: String, _front_color: i16, _back_color: i16, _flags: Vec<i32>) {}
 
-        getmaxyx(self.screen, &mut max_y, &mut max_x);
-
-        let mut _pad_lines_result = 0;
-        let mut _pad_lines_output = 0;
-
-        // set result output type(stdout/stderr/output)
-        let output = self.get_output(output_type);
-        let output_text = output.split("\n");
-
-        for _output_line in output_text {
-            _pad_lines_result +=
-                get_pad_lines(_output_line.to_string(), max_x - (::HISTORY_WIDTH + 2));
-        }
-
-        for _output_line in self.result_diff_output.clone().split("\n") {
-            _pad_lines_output +=
-                get_pad_lines(_output_line.to_string(), max_x - (::HISTORY_WIDTH + 2));
-        }
-
-        self.pad_lines = cmp::max(_pad_lines_result, _pad_lines_output + 1);
-        self.pad = newpad(self.pad_lines.clone(), max_x - (::HISTORY_WIDTH + 2));
-    }
-
-    pub fn print_plain(&mut self, output: String) {
+    // @TODO
+    // いらないのでは？？
+    // print(今のprint_watch) だけにして、これにリバースとか色の指定オプションを付与するようにすればいいだけな気がしてきた。
+    // → 消そう！
+    pub fn print_plain_data(&mut self, output: String) {
         let output_text = output.split("\n");
 
         for line in output_text {
@@ -69,7 +40,7 @@ impl WatchPad {
         }
     }
 
-    pub fn print_watch_char(&mut self, _char: String, _reverse: bool, _color_code: i16) {
+    pub fn print_watch_data(&mut self, _char: String, _reverse: bool, _color_code: i16) {
         if _reverse {
             wattron(self.pad, A_REVERSE());
             self.print_char_to_color_pair(_char, _color_code);
@@ -89,32 +60,12 @@ impl WatchPad {
         }
     }
 
-    fn print_char_to_color(&mut self, _char: String, _front_color: i16, _back_color: i16) {}
-
-    fn get_output(&mut self, output_type: i32) -> String {
-        let mut output = String::new();
-        match output_type {
-            ::IS_OUTPUT => output = self.result.output.clone(),
-            ::IS_STDOUT => output = self.result.stdout.clone(),
-            ::IS_STDERR => output = self.result.stderr.clone(),
-            _ => {}
-        };
-        return output;
-    }
-
     pub fn draw_output_pad(&mut self) {
         let mut max_x = 0;
         let mut max_y = 0;
         getmaxyx(self.screen, &mut max_y, &mut max_x);
-        prefresh(
-            self.pad,
-            self.pad_position,
-            0,
-            2,
-            0,
-            max_y - 1,
-            max_x - (::HISTORY_WIDTH + 2),
-        );
+
+        self.prefresh(max_y, max_x);
     }
 
     pub fn scroll_up(&mut self) {
@@ -124,15 +75,7 @@ impl WatchPad {
 
         if self.pad_lines > max_y && self.pad_position > 0 {
             self.pad_position -= 1;
-            prefresh(
-                self.pad,
-                self.pad_position,
-                0,
-                2,
-                0,
-                max_y - 1,
-                max_x - (::HISTORY_WIDTH + 2),
-            );
+            self.prefresh(max_y, max_x);
         }
     }
 
@@ -143,15 +86,7 @@ impl WatchPad {
 
         if self.pad_lines > max_y && self.pad_position < (self.pad_lines - max_y + 2 - 1) {
             self.pad_position += 1;
-            prefresh(
-                self.pad,
-                self.pad_position,
-                0,
-                2,
-                0,
-                max_y - 1,
-                max_x - (::HISTORY_WIDTH + 2),
-            );
+            self.prefresh(max_y, max_x);
         }
     }
 
@@ -159,7 +94,12 @@ impl WatchPad {
         let mut max_x = 0;
         let mut max_y = 0;
         getmaxyx(self.screen, &mut max_y, &mut max_x);
+
         resizeterm(max_y, max_x);
+        self.prefresh(max_y, max_x);
+    }
+
+    fn prefresh(&mut self, max_y: i32, max_x: i32) {
         prefresh(
             self.pad,
             self.pad_position,
@@ -174,28 +114,6 @@ impl WatchPad {
     pub fn exit(&self) {
         endwin();
     }
-}
-
-// get pad lines from string
-// @TODO: move mod.rs
-fn get_pad_lines(_string: String, _width: i32) -> i32 {
-    let char_vec: Vec<char> = _string.chars().collect();
-    let mut _char_count = 0;
-    let mut _line_count = 1;
-
-    for ch in char_vec {
-        if ch.to_string().len() > 1 {
-            _char_count += 2;
-        } else {
-            _char_count += 1;
-        }
-
-        if _char_count == _width {
-            _line_count += 1;
-            _char_count = 0;
-        }
-    }
-    return _line_count;
 }
 
 // @TODO:
