662bc49f9d5c4513be6793dc1919b178c2e285a7
blacknon <blacknon@orebibou.com>
2022-02-11 15:19:55 +0900

    update. test actions.

---
+++ scripts/version.rs
@@ -0,0 +1,7 @@
+// Copyright (c) 2021 Blacknon. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
+fn main() {
+  println!("{}", env!("CARGO_PKG_VERSION"));
+}
