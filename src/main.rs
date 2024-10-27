5b0b0a45789192a2559ba937e661d9fe3d8383fd
blacknon <blacknon@orebibou.com>
2024-10-27 23:41:49 +0900

    update. とりあえずそれっぽく動くとこまで(ps auxでも操作のカクつきが軽減された)

--- src/main.rs
+++ src/main.rs
@@ -54,6 +54,7 @@ extern crate shell_words;
 extern crate similar;
 extern crate termios;
 extern crate termwiz;
+extern crate tokio;
 extern crate nix;
 extern crate ratatui as tui;
 extern crate unicode_width;
