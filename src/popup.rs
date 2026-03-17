4087fc67ecb53daf358a995a0ec31cff7d773367
Blacknon <blacknon@orebibou.com>
2026-03-17 22:31:13 +0900

    update. upgrade lib.

--- src/popup.rs
+++ src/popup.rs
@@ -2,7 +2,6 @@
 // Use of this source code is governed by an MIT license
 // that can be found in the LICENSE file.
 
-use ratatui::style::Stylize;
 use tui::{
     layout::Rect,
     prelude::Line,
